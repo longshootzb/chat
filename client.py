76094e0deb92b3709b974dd3b068b11d81152f8b
zhangbo <13571879908@163.com>
2017-05-25 20:16:25 +0800

    nnnn

--- client.py
+++ client.py
@@ -1,55 +1,49 @@
-# telnet program example
-import socket, select, string, sys
+import socket, select, threading, sys;
+
+host = socket.gethostname()
+
+addr = (host, 5963)
 
 
-def prompt():
-    sys.stdout.write('<You> ')
-    sys.stdout.flush()
+def conn():
+    s = socket.socket()
+    s.connect(addr)
+    return s
 
 
-# main function
-if __name__ == "__main__":
+def lis(s):
+    my = [s]
+    while True:
+        r, w, e = select.select(my, [], [])
+        if s in r:
+            try:
+                print s.recv(1024)
+            except socket.error:
+                print 'socket is error'
+                exit()
 
-    if (len(sys.argv) < 3):
-        print 'Usage : python telnet.py hostname port'
-        sys.exit()
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+def talk(s):
+    while True:
+        try:
+            info = raw_input()
+        except Exception, e:
+            print 'can\'t input'
+            exit()
+        try:
+            s.send(info)
+        except Exception, e:
+            print e
+            exit()
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
 
-    # connect to remote host
-    try:
-        s.connect((host, port))
-    except:
-        print 'Unable to connect'
-        sys.exit()
+def main():
+    ss = conn()
+    t = threading.Thread(target=lis, args=(ss,))
+    t.start()
+    t1 = threading.Thread(target=talk, args=(ss,))
+    t1.start()
 
-    print 'Connected to remote host. Start sending messages'
-    prompt()
 
-    while 1:
-        rlist = [sys.stdin, s]
-
-        # Get the list sockets which are readable
-        read_list, write_list, error_list = select.select(rlist, [], [])
-
-        for sock in read_list:
-            # incoming message from remote server
-            if sock == s:
-                data = sock.recv(4096)
-                if not data:
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else:
-                    # print data
-                    sys.stdout.write(data)
-                    prompt()
-
-            # user entered a message
-            else:
-                msg = sys.stdin.readline()
-                s.send(msg)
-                prompt()
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,68 +1,87 @@
-# Tcp Chat server
+import socket, select, thread;
 
-import socket, select
+host = socket.gethostname()
+port = 5963
+addr = (host, port)
 
-
-# Function to broadcast chat messages to all connected clients
-def broadcast_data(sock, message):
-    # Do not send the message to master socket and the client who has send us the message
-    for socket in CONNECTION_LIST:
-        if socket != server_socket and socket != sock:
-            try:
-                socket.send(message)
-            except:
-                # broken socket connection may be, chat client pressed ctrl+c for example
-                socket.close()
-                CONNECTION_LIST.remove(socket)
+inputs = []
+fd_name = {}
 
 
-if __name__ == "__main__":
+def who_in_room(w):
+    name_list = []
+    for k in w:
+        name_list.append(w[k])
 
-    # List to keep track of socket descriptors
-    CONNECTION_LIST = []
-    RECV_BUFFER = 4096  # Advisable to keep it as an exponent of 2
-    PORT = 5000
+    return name_list
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
-    server_socket.listen(10)
 
-    # Add server socket to the list of readable connections
-    CONNECTION_LIST.append(server_socket)
+def conn():
+    print 'runing'
+    ss = socket.socket()
+    ss.bind(addr)
+    ss.listen(5)
 
-    print "Chat server started on port " + str(PORT)
+    return ss
 
-    while 1:
-        # Get the list sockets which are ready to be read through select
-        read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
 
-        for sock in read_sockets:
-            # New connection
-            if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
-                sockfd, addr = server_socket.accept()
-                CONNECTION_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
+def new_coming(ss):
+    client, add = ss.accept()
+    print 'welcome %s %s' % (client, add)
+    wel = '''''welcome into the talking room . 
+    please decide your name.....'''
+    try:
+        client.send(wel)
+        name = client.recv(1024)
+        inputs.append(client)
+        fd_name[client] = name
 
-                broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
+        nameList = "Some people in talking room, these are %s" % (who_in_room(fd_name))
+        client.send(nameList)
 
-            # Some incoming message from a client
+    except Exception, e:
+        print e
+
+
+def server_run():
+    ss = conn()
+    inputs.append(ss)
+
+    while True:
+        r, w, e = select.select(inputs, [], [])
+        for temp in r:
+            if temp is ss:
+                new_coming(ss)
             else:
-                # Data recieved from client, process it
+                disconnect = False
                 try:
-                    # In Windows, sometimes when a TCP program closes abruptly,
-                    # a "Connection reset by peer" exception will be thrown
-                    data = sock.recv(RECV_BUFFER)
-                    if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
+                    data = temp.recv(1024)
+                    data = fd_name[temp] + ' say : ' + data
+                except socket.error:
+                    data = fd_name[temp] + ' leave the room'
+                    disconnect = True
 
-                except:
-                    broadcast_data(sock, "Client (%s, %s) is offline" % addr)
-                    print "Client (%s, %s) is offline" % addr
-                    sock.close()
-                    CONNECTION_LIST.remove(sock)
-                    continue
+                if disconnect:
+                    inputs.remove(temp)
+                    print data
+                    for other in inputs:
+                        if other != ss and other != temp:
+                            try:
+                                other.send(data)
+                            except Exception, e:
+                                print e
+                    del fd_name[temp]
 
-    server_socket.close()
+                else:
+                    print data
+
+                    for other in inputs:
+                        if other != ss and other != temp:
+                            try:
+                                other.send(data)
+                            except Exception, e:
+                                print e
+
+
+if __name__ == '__main__':
+    server_run()
